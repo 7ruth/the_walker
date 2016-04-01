9e7de82cc7674c654452d1d3e2f916b5c21bd9a1
Martin Terskin <martinas.terskin@gmail.com>
2016-04-01 10:10:30 -0400

    added js to make the man walk from one side of the screen to the other

--- walker.js
+++ walker.js
@@ -5,6 +5,38 @@ var walker = document.getElementById('walker');
 var leftBorder = 0;
 var rightBorder = window.innerWidth - walker.offsetWidth;
 
+var distance = 1;
+var direction = 0;
+
+console.log(window.innerWidth);
+
+window.setInterval(function(){
+
+
+
+  walker.style.right= (distance) + 'px';
+
+
+        if (direction === 0) {
+          distance += window.innerWidth/500;
+        } else {
+          distance -= window.innerWidth/500;
+        }
+
+        if (distance < window.innerWidth*(.1)) {
+          direction = 0;
+        } else if (distance > window.innerWidth*(.85)) {
+          direction = 1;
+        }
+
+console.log(walker.style.right);
+console.log(direction);
+
+}, 10);
+
+
+
+console.log(rightBorder);
 // Have the stick figure start at the right border and start walking left
 // When he crosses the left border, have him turn around and start walking right (and vice versa)
 // Bonus: Make him turn around when you click on him
